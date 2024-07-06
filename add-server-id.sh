026664a4dc03d3d32a19fd37748d2fec20ea4b31
이승구 <leesg107@naver.com>
2024-07-06 23:55:55 +0900

    fix: 오타 수정

--- add-server-id.sh
+++ add-server-id.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-OCTETS=(`hostname - i | tr -s '.' ''`)
+OCTETS=(`hostname -i | tr -s '.' ' '`)
 
 MYSQL_SERVER_ID=`expr ${OCTETS[2]} \* 256 + ${OCTETS[3]}`
 echo "server-id=$MYSQL_SERVER_ID" >> /etc/mysql/mysql.conf.d/mysqld.cnf
