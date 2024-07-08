5a2b695fdfceef73b0b7ae6a912b8d4465e01e1b
이승구 <leesg107@naver.com>
2024-07-08 13:39:07 +0900

    feat: slave read-only 옵션 추가 및 vim 설치 추가

--- add-server-id.sh
+++ add-server-id.sh
@@ -3,3 +3,8 @@ OCTETS=(`hostname -i | tr -s '.' ' '`)
 
 MYSQL_SERVER_ID=`expr ${OCTETS[2]} \* 256 + ${OCTETS[3]}`
 echo "server-id=$MYSQL_SERVER_ID" >> /etc/mysql/mysql.conf.d/mysqld.cnf
+
+if [ -z "$MYSQL_MASTER" ]; then
+    echo "read_only=1" >> /etc/mysql/mysql.conf.d/mysqld.cnf
+    echo "Slave is now read-only"
+fi
